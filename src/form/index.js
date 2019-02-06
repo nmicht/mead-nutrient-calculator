872b4d0564725088f81557de0d4f8a6464cc7058
Mihelle Torres <lic.nancy.torres@gmail.com>
2019-02-06 00:59:25 -0500

    Remove innecesary data from the state

--- src/form/index.js
+++ src/form/index.js
@@ -6,12 +6,13 @@ import './index.css';
 
 export default class Form extends React.Component {
 
-  renderInput(item) {
+  renderInput(item, value) {
     if (item.type === 'radio' || item.type === 'checkbox') {
       return (
         <Radio
           key={item.id}
           {...item}
+          value={value}
           onChange={(e) => item.onChange(e)}
         />
       );
@@ -20,6 +21,7 @@ export default class Form extends React.Component {
         <Select
           key={item.id}
           {...item}
+          value={value}
           onChange={(e) => item.onChange(e)}
         />
       );
@@ -28,6 +30,7 @@ export default class Form extends React.Component {
         <Input
           key={item.id}
           {...item}
+          value={value}
           name={item.id}
           onChange={(e) => item.onChange(e)}
         />
@@ -36,7 +39,7 @@ export default class Form extends React.Component {
   }
 
   render() {
-    const inputs = this.props.items.map(item => this.renderInput(item));
+    const inputs = this.props.fields.map(item => this.renderInput(item, this.props.inputs[item.id]));
 
     return (
       <div>
